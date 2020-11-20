a652512bec70715e9a4fe041597006d3c527f3fd
mohammad sajjad kooshkestani <kooshkestani@outlook.com>
2020-11-20 19:54:55 +0330

    Fix some bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
-
+  target: 'static',
+  router: {
+    base: '/Nuxt-with-pug/'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'nuxt-with-pug',
